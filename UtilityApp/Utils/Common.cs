f9213f46b0c029dc759ad7217bab33bf16a90304
Anthony <dinhhung09138@gmail.com>
2018-03-05 14:20:24 +0700

    Update constant class.

--- UtilityApp/Utils/Common.cs
+++ UtilityApp/Utils/Common.cs
@@ -12,6 +12,6 @@ namespace Utils
         /// Session name for user data after login
         /// </summary>
         public static readonly string SESSION_LOGIN_NAME = "UserProfile";
-
+        
     }
 }
---
+++ UtilityApp/Utils/Constant/FormatValue.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utils.Constant
+{
+    /// <summary>
+    /// Constant Data format value
+    /// </summary>
+    public class FormatValue
+    {
+        /// <summary>
+        /// Date and time format
+        /// dd/MM/yyyy HH:mm:ss
+        /// </summary>
+        public static readonly string FULL_DATE_FORMAT = "dd/MM/yyyy HH:mm:ss";
+
+        /// <summary>
+        /// Date format
+        /// dd/MM/yyyy
+        /// </summary>
+        public static readonly string DATE_FORMAT = "dd/MM/yyyy";
+
+        /// <summary>
+        ///  Number format
+        ///  #,##0
+        /// </summary>
+        public static readonly string NUMBER_FORMAT = "#,##0";
+
+        /// <summary>
+        /// Price format
+        /// #,##0.##
+        /// </summary>
+        public static readonly string PRICE_FORMAT = "#,##0.##";
+    }
+}
